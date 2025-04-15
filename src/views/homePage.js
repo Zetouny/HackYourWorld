8fbf1a3614d8c051dc21d438b72b3ef3a8c92fbc
A. Zetouny <Zet_Ahmad@hotmail.com>
2025-04-16 00:53:18 +0200

    HomePage - Globe and new buttons

--- src/views/homePage.js
+++ src/views/homePage.js
@@ -5,6 +5,14 @@ export const createHomePage = () => {
   main.innerHTML = String.raw`
     <h1 class="page-title">HackYourWorld</h1>
     
+    <p>Your one-stop destination for exploring countries around the globe! Instantly access key details about any country, all powered by daily updated data. Start discovering the world today!</p>
+    <p><strong>Your mission:</strong> Hack the secrets of the world! Choose your weapon wisely</p>
+
+    <div class="button-container">
+      <a href="#" role="button">Hack My Location!</a>
+      <a href="#" role="button">Browse Countries</a>
+    </div>
+
     <div id="globe-container">
       <canvas id="globe" width="1000" height="1000"></canvas>
     </div>
